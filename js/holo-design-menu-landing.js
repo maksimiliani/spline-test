6b71d032f7f8a6a1d906b672fff67b79d35be054
maksimiliani <macseg@gmail.com>
2021-08-12 01:37:03 +0300

    Update holo-design-menu-landing.js

--- js/holo-design-menu-landing.js
+++ js/holo-design-menu-landing.js
@@ -37,7 +37,6 @@ function update_header(colourr) {
 
   header_el = $(".navbar-master");
   bodyRect = document.body.getBoundingClientRect();
-  console.log(document.body.getBoundingClientRect());
 
   for (var i=0; i < sections_color.length; i++) {
     var offset1 = sections_color[i].getBoundingClientRect().top - bodyRect.top - header_el[0].offsetHeight;
